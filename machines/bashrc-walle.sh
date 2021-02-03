1e4c0ea40805e2b152c043c39b55540c0200e3ac
Saejin Mahlau-Heinert <saejinmh@va.wagner.com>
2021-02-03 15:42:59 -0500

    Update walle bashrc to reflect elementary OS move

--- machines/bashrc-walle.sh
+++ machines/bashrc-walle.sh
@@ -9,6 +9,7 @@ source "$HOME/.aliases"
 export EDITOR=vim
 #export PATH="$PATH:/opt/powerline"
 
+POWERLINE_VERSION="go"
 source $HOME/.userfiles/bashrc
 
 source $HOME/.vpn/control.sh
@@ -21,11 +22,7 @@ if command -v pyenv 1>/dev/null 2>&1; then
   eval "$(pyenv init -)"
 fi
 
-source /opt/hub/etc/hub.bash_completion.sh
 eval "$(hub alias -s)"
-
-# added by travis gem
-[ -f /home/saejin/.travis/travis.sh ] && source /home/saejin/.travis/travis.sh
 
 #THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
 export SDKMAN_DIR="/home/saejin/.sdkman"
@@ -35,14 +32,10 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-export PATH="$(yarn global bin):$PATH"
+#export PATH="$(yarn global bin):$PATH"
 #eval "$(grunt --completion=bash)"
 
 export VAGRANT_SSH_KEY=$HOME/.ssh/id_rsab
 export CAS_MACHINE=LINUX
 export TERM=xterm-256color
 export GRADLE_USER_HOME=$GRADLE_HOME
-
-function ckan() {
-  x11docker --share ${1:-/mnt/work/games/ksp-modded} -- -- michionlion/kerbal-ckan:latest ckan gui
-}
