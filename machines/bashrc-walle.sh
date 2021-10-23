ebe75b4ff8fd339ebf33c097b7b42f843abd7dd2
Saejin Mahlau-Heinert <saejinmh@va.wagner.com>
2021-10-23 13:40:36 -0400

    Update bashrc-walle

--- machines/bashrc-walle.sh
+++ machines/bashrc-walle.sh
@@ -6,18 +6,10 @@
 # This script should be symlinked to $HOME/.bashrc
 
 export DOTNET_CLI_TELEMETRY_OPTOUT=true
+export EDITOR=vim
 
 source "$HOME/.aliases"
 
-export EDITOR=vim
-#export PATH="$PATH:/opt/powerline"
-
-POWERLINE_VERSION="go"
-source $HOME/.userfiles/bashrc
-
-source $HOME/.vpn/control.sh
-
-[[ -s /usr/share/autojump/autojump.sh ]] && source /usr/share/autojump/autojump.sh
 
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
@@ -27,10 +19,6 @@ if command -v pyenv 1>/dev/null 2>&1; then
 fi
 
 eval "$(hub alias -s)"
-
-#THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
-export SDKMAN_DIR="/home/saejin/.sdkman"
-[[ -s "/home/saejin/.sdkman/bin/sdkman-init.sh" ]] && source "/home/saejin/.sdkman/bin/sdkman-init.sh"
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
@@ -43,3 +31,19 @@ export VAGRANT_SSH_KEY=$HOME/.ssh/id_rsab
 export CAS_MACHINE=LINUX
 export TERM=xterm-256color
 export GRADLE_USER_HOME=$GRADLE_HOME
+
+POWERLINE_VERSION="go"
+source $HOME/.userfiles/bashrc
+
+source $HOME/.vpn/control.sh
+
+[[ -s /usr/share/autojump/autojump.sh ]] && source /usr/share/autojump/autojump.sh
+
+#THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
+export SDKMAN_DIR="/home/saejin/.sdkman"
+[[ -s "/home/saejin/.sdkman/bin/sdkman-init.sh" ]] && source "/home/saejin/.sdkman/bin/sdkman-init.sh"
+
+# ---- BEGIN niera GENERATED SECTION ----
+export PATH="$PATH:~/.niera/bin"
+. ~/.niera/lib/niera-completion.sh
+# ---- END niera GENERATED SECTION ----
